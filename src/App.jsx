4744f3c5ef01ff61f2814d985896ddae9fa3dc76
Matt Ha <matthewha1993@gmail.com>
2020-08-24 21:35:54 -0800

    Changes to boiler plate and basic functionality: add delete and edit modal state.

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import './App.css';
+import TodoList from './components/todo_list.jsx';
+import FilterButton from "./components/filter_button.jsx";
+import TaskForm from "./components/task_form.jsx";
+import { nanoid } from "nanoid";
+
+const App = props => {
+  const { taskData } = props;
+  const [tasks, setTasks] = useState(taskData);
+  const tasksFormattedNoun = tasks.length !== 1 ? 'tasks' : 'task';
+
+  const toggleCompleted = id => {
+    const updatedTasks = tasks.map(task => {
+      if (id === task.id) {
+        // override data with switched completed attribute
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
+  
+  const createTask = name => {
+    const newTask = { 
+      name, 
+      id: `task=${nanoid()}`, 
+      isCompleted: false, 
+    };
+    setTasks([...tasks, newTask]);
+  };
+  
+  const deleteTask = id => {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  };
+
+  const editTask = (id, newTask) => {
+    const editedTaskList = tasks.map(task => {
+      if (id === task.id) {
+        // replace task with updated data
+        return newTask;
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+  return (
+    <div className="todoapp stack-large">
+      <TaskForm createTask={createTask} />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">
+        {tasks.length} {tasksFormattedNoun} remaining
+      </h2>
+      <TodoList 
+        taskData={tasks}
+        toggleCompleted={toggleCompleted}
+        editTask={editTask}
+        deleteTask={deleteTask}
+      />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const TASK_DATA = [
+  { id: "todo-0", name: "Eat", isCompleted: true },
+  { id: "todo-1", name: "Sleep", isCompleted: false },
+  { id: "todo-2", name: "Repeat", isCompleted: false }
+];
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App taskData={TASK_DATA} />
   </React.StrictMode>,
   document.getElementById('root')
 );
