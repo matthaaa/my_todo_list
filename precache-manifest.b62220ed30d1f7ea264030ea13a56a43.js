bca764c2519f4401b75c7255db123e858f658968
Matt Ha <matthewha1993@gmail.com>
2020-08-28 03:29:53 -0800

    Updates

---
+++ precache-manifest.b62220ed30d1f7ea264030ea13a56a43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c35570eadb9b6c183b8401b6999c70",
+    "url": "/my_todo_list/index.html"
+  },
+  {
+    "revision": "330033d41ca2deb3e743",
+    "url": "/my_todo_list/static/css/2.6d52cc7f.chunk.css"
+  },
+  {
+    "revision": "b45e514be7f1f013acac",
+    "url": "/my_todo_list/static/css/main.a6b4e740.chunk.css"
+  },
+  {
+    "revision": "330033d41ca2deb3e743",
+    "url": "/my_todo_list/static/js/2.8899bfec.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/my_todo_list/static/js/2.8899bfec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b45e514be7f1f013acac",
+    "url": "/my_todo_list/static/js/main.d942ffe3.chunk.js"
+  },
+  {
+    "revision": "a81dce39974769953520",
+    "url": "/my_todo_list/static/js/runtime-main.95e5feef.js"
+  }
+]);
