120877732b9b55b69d207b1ca6fee7fa82cbb719
Matt Ha <matthewha1993@gmail.com>
2020-09-03 18:19:23 -0800

    Prop validations for button component

--- src/components/generic/button.jsx
+++ src/components/generic/button.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import PropTypes from 'prop-types';
+
 import Button from 'react-bootstrap/Button';
 
 // TODO(WIP): Finish and add to all locations where buttons are.
@@ -10,12 +12,22 @@ const CustomButton = (props) => {
   } = props;
 
   return (
-    <Button 
+    <Button
       onClick={onClick}
       style={style}>
       {label}
     </Button>
   );   
+};
+
+CustomButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+  style: PropTypes.object,
+};
+
+CustomButton.defaultProps = {
+  style: null,
 };
 
 export default CustomButton;
